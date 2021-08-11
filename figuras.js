af9967b7766365df6be0b330e57d13f2e376cc5f
AndresPC2 <cristian.escobarc@campusucc.edu.co>
2021-08-11 02:43:38 +0200

    agregando boostrap al repositorio y agragando cambios

--- figuras.js
+++ figuras.js
@@ -1,25 +1,24 @@
 
 
-console.group("cuadrados")
+//console.group("cuadrados")
 // Código del cuadrado
-const ladoCuadrado = 5
-const perimetroCuadrado = ladoCuadrado *4 
-const areacuadrado = ladoCuadrado * ladoCuadrado
+//const ladoCuadrado = 5
 
 
-console.log(`los lados de cuadrado miden: ${ladoCuadrado} cm `) 
-console.log(`El perímetro del cuadrado mide:  ${perimetroCuadrado} cm `)
-console.log(`el area del cuadrado mide: ${areacuadrado} cm²`)
-console.groupEnd()
+//console.log(`los lados de cuadrado miden: ${ladoCuadrado} cm `) 
+//console.log(`El perímetro del cuadrado mide:  ${perimetroCuadrado} cm `)
+//console.log(`el area del cuadrado mide: ${areacuadrado} cm²`)
+//console.groupEnd()
+
+
+
 // Código del triangulo
-console.group("Triangulo")
+/* console.group("Triangulo")
 const ladoTriangulo1 = 6
 const ladoTriangulo2 = 6
 const baseTriangulo = 4
 const alturaTriangulo = 5.5
 
-const perimetroTriangulo = baseTriangulo + ladoTriangulo2 + ladoTriangulo1
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2
 console.log(`Los lados del triangulo miden  ${ladoTriangulo1} cm`)
 console.log(`Los lados del triangulo miden  ${ladoTriangulo2} cm`)
 console.log(`Los lados del triangulo miden  ${baseTriangulo} cm`)
@@ -28,21 +27,32 @@ console.log(`El perimetro del triangulo   ${perimetroTriangulo} cm`)
 console.log(`El area del triangulo   ${areaTriangulo} cm`)
 
 console.groupEnd()
+*/
 
 
+
+//circulo
+
+
+
+
+/* 
 console.group("Circulo")
-
-const RadioCirculo = 4
-const DiametroCirculo = RadioCirculo * 2 
-const PI = Math.PI 
-const perimetroCirculo  = DiametroCirculo * PI
-const areaCirculo = (RadioCirculo * RadioCirculo) *2 
-
 console.log(`el radio del circulo es ${RadioCirculo} cm `)
 console.log(`el diametro del circulo es ${DiametroCirculo} cm `)
 console.log(`pi es ${PI} cm `)
 console.log(`el perimetro del circulo es ${perimetroCirculo} cm `)
 console.log(`el circulo area del circulo es ${areaCirculo} cm `)
-
-
 console.groupEnd()
+ */
+//cuadrado 
+const perimetroCuadrado = lado => lado *4 ;
+const areacuadrado = lado => lado * lado ;
+//triangulo 
+const perimetroTriangulo = (base, lado1, lado2) => base+ lado1 + lado2
+const areaTriangulo = (base,altura) => (base * altura) / 2
+//circulo
+const DiametroCirculo = radio => radio * 2 
+const PI = Math.PI 
+const perimetroCirculo  = radio => DiametroCirculo(radio) * PI
+const areaCirculo = radio => (radio  * radio) *2 
